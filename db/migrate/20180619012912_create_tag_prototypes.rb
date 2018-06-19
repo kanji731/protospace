31613e665014f3729025852e7c267d18f669312e
kanji731 <nagao@NagaonoMacBook-puro.local>
2018-06-19 10:37:14 +0900

    Add model tag & tag_prototype

---
+++ db/migrate/20180619012912_create_tag_prototypes.rb
@@ -0,0 +1,10 @@
+class CreateTagPrototypes < ActiveRecord::Migration
+  def change
+    create_table :tag_prototypes do |t|
+      t.references :prototype, foreign_key: true
+      t.references :tag, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
